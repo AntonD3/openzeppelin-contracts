e13aea3a30419bb0e8643c97f6aa1e808b4f159a
AntonD3 <antonv.dyadyuk@gmail.com>
2023-06-29 13:34:33 +0200

    zksolc 1.3.12

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,20 +2,32 @@ import '@matterlabs/hardhat-zksync-solc'
 import '@matterlabs/hardhat-zksync-verify'
 
 module.exports = {
-    zksolc: {
-        version: "1.3.10",
-        compilerSource: "binary",
-        settings: {},
-    },
     networks: {
+        zkSyncLocalhost: {
+            url: 'http://localhost:3050',
+            ethNetwork: 'http://localhost:8545',
+            zksync: true,
+        },
         zkSyncTestnet: {
             url: "https://testnet.era.zksync.dev",
             ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
             zksync: true,
             verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
         },
+        zkSyncMainnet: {
+            url: 'https://mainnet.era.zksync.io',
+            ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            zksync: true,
+            verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification'
+        },
     },
+    defaultNetwork: 'zkSyncLocalhost',
     solidity: {
         version: '0.7.4',
+    },
+    zksolc: {
+        version: "1.3.12",
+        compilerSource: "binary",
+        settings: {},
     },
 };
